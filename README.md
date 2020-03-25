c9f0dd2d6baec18ff4ee1b87816e49cffebd3c8b
linxuhao <linxuhao84@gmail.com>
2020-03-25 20:32:16 +0100

    comment API

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PersonalPageFirebaseFunctions
 API for personal page using Firebase
+
+# Tutorial from : 
+Firebase cloud functions: https://medium.com/codingthesmartway-com-blog/introduction-to-firebase-cloud-functions-c220613f0ef
+
+Reading and writing data firebase: https://firebase.google.com/docs/database/web/read-and-write
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,24 @@
 import * as functions from 'firebase-functions';
+import { database, initializeApp } from "firebase-admin";
+
+initializeApp(functions.config().firebase);
+const db = database();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
- export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
+export const helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+
+export const addComment = functions.https.onRequest((request, response) => {
+    db.ref('comments').push({ comment: request.body.comment, timestamp: request.body.timestamp });
+    response.send(request.body.comment);
+});
+
+export const getComments = functions.https.onRequest((request, response) => {
+    db.ref('comments').orderByKey().once('value').then((snapshot) => {
+        response.send(snapshot.val);
+    }).catch(() => response.status(500).send('Error'));
+});
+
